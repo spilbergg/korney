6c2edd858fced85a9f5bd83335741f2e5bb89298
spilbergg <spilbergg@yandex.ru>
2022-09-15 00:47:03 +0300

    corrected from pep8

---
+++ main/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+
+from .customwidget import DateSelectorWidget
+from .models import Author, Book, Genre, ImageBook, PersonReader
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def clean_name_book_rus(self):
+        return self.cleaned_data['name_book_rus'].lower()
+
+
+class BookFormGenre(forms.ModelForm):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class BookFormAuthors(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+class ImageBookForm(forms.ModelForm):
+    class Meta:
+        model = ImageBook
+        fields = '__all__'
+
+
+class PersonReaderForm(forms.ModelForm):
+    class Meta:
+        model = PersonReader
+        fields = '__all__'
+
+        widgets = {
+            'date_birthday': DateSelectorWidget,
+            'person_get_book': DateSelectorWidget,
+        }
+
+
+class Author_form(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = '__all__'
